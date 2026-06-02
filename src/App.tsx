a02e62e80f7a43f77bb53d5d630d1ea0375c0979
Iman <ailmanmosque8@gmail.com>
2026-06-02 07:03:51 +0100

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter basename={import.meta.env.BASE_URL} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<QuranReader />} />
             <Route path="/audio" element={<Index />} />
